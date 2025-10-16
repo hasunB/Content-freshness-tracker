0d78f24e9781c7cc73d8106364beed443b0a84b5
hasunB <hasunbandara17@gmail.com>
2025-10-17 01:11:47 +0530

    feat : added filter functions (reviewed, unreviewd), chart syncing wuth real data, and UI updates

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -17,6 +17,7 @@ jQuery(function($){
                 var newBtn = $('<button class="review-action-btn btn-reviewed" data-post-id="' + id + '">' +
                                 '<i class="fa-solid fa-check-double"></i>&nbsp;&nbsp;Reviewed' +
                                '</button>');
+                btn.closest('.post-item').removeClass('fr-unreviewed').addClass('fr-reviewed');
                 btn.replaceWith(newBtn);
             } else {
                 btn.prop('disabled', false);
@@ -43,11 +44,45 @@ jQuery(function($){
                 var newBtn = $('<button class="review-action-btn btn-review" data-post-id="' + id + '">' +
                                 '<i class="fa-solid fa-check"></i>&nbsp;&nbsp;Review' +
                                '</button>');
+                btn.closest('.post-item').removeClass('fr-reviewed').addClass('fr-unreviewed');
                 btn.replaceWith(newBtn);
             } else {
                 btn.prop('disabled', false);
                 alert(resp && resp.data ? resp.data : 'Response Error');
             }
         });
+    });
+
+    $(document).on('click', '.theme-filter-btn', function() {
+        var filter = $(this).data('filter');
+        var container = $(this).closest('.theme-stale-content');
+        var contentBox = container.find('.theme-content-box');
+        var noPostsMessage = contentBox.find('.no-posts-box');
+        var pagination = container.find('.theme-pagination-box');
+
+        // Active button class
+        container.find('.theme-filter-btn').removeClass('active');
+        $(this).addClass('active');
+
+        var posts = contentBox.find('.post-item');
+        var visiblePosts;
+
+        if (filter === 'all') {
+            posts.show();
+            visiblePosts = posts;
+        } else if (filter === 'reviewed') {
+            posts.hide();
+            visiblePosts = contentBox.find('.fr-reviewed').show();
+        } else if (filter === 'unreviewed') {
+            posts.hide();
+            visiblePosts = contentBox.find('.fr-unreviewed').show();
+        }
+
+        if (visiblePosts.length === 0) {
+            noPostsMessage.css('display', 'flex');
+            pagination.css('display', 'none');
+        } else {
+            noPostsMessage.hide();
+        }
     });
 });
